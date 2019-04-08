f7c6373e760271f4588183d2ede66fe1a9d933fd
Connor Kam-Cheong <connorkc42@gmail.com>
2019-04-08 12:33:43 +0100

    Add test for author search returning multiple books

--- src/test/scala/com/company/library/LibrarySpec.scala
+++ src/test/scala/com/company/library/LibrarySpec.scala
@@ -16,4 +16,17 @@ class LibrarySpec extends FunSuite {
     val library = new Library
     library.searchAuthor("Sebold") shouldBe Set(Book("Lovely Bones,The", "Sebold, Alice", "stniskzb"))
   }
+
+  test("Search by partial Author (multiple books)") {
+    val library = new Library
+    val result = Set(
+      Book("Da Vinci Code,The", "Brown, Dan", "pidtkl"),
+      Book("Angels and Demons", "Brown, Dan", "mayiwrko"),
+      Book("Lost Symbol,The", "Brown, Dan", "ewvxsoql"),
+      Book("Deception Point", "Brown, Dan", "wixnjrdb"),
+      Book("Digital Fortress", "Brown, Dan", "vwgikppxt")
+    )
+
+    library.searchAuthor("Dan") shouldBe result
+  }
 }
